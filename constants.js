7a50e17f731b3ecbeb797c7f02d202e5810f174c
AlSirang <uddinbashir1999@gmail.com>
2022-12-08 00:20:53 +0500

    fix: add required configs

---
+++ constants.js
@@ -0,0 +1,6 @@
+// already exposed private key. do not use it for personal use
+const PRIVATE_KEY =
+  "6b911fd37cdf5c81d4c0adb1ab7fa822ed253ab0ad9aa18d77257c88b29b718e";
+const PUBLIC_KEY_WITHOUT_OX = "16bB6031CBF3a12B899aB99D96B64b7bbD719705";
+
+module.exports = { PRIVATE_KEY, PUBLIC_KEY_WITHOUT_OX };
